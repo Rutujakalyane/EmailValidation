9f2a1e5add1b06854239578491e49bbbeefaa86f
Rutujakalyane <60542373+Rutujakalyane@users.noreply.github.com>
2020-04-11 17:33:03 +0530

    ADDED : Second mandatory part of email

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -1,14 +1,14 @@
-function firstpart()
+function emailRegex()
 {
 echo "Enter : "
 read word
 
-firstword="^[a-zA-Z0-9]{2}$"
-if [[ $word =~ $firstword ]]
+emailPattern2="^[@][a-zA-Z]{1,}"
+if [[ $word =~ $emailPattern2 ]]
 then 
         echo valid;
 else
         echo "invalid"
 fi
 }
-firstpart
+emailRegex
